16833a58ceed6c6b30a27a4a5ffcad6ff2da41e6
subhasis4502 <subhasis4502@gmail.com>
2025-04-15 23:32:22 +0530

    Upading icon size

--- src/components/about/AboutPage.jsx
+++ src/components/about/AboutPage.jsx
@@ -94,6 +94,8 @@ const AboutPage = () => {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
+                  width="48"
+                  height="48"
                   fill="currentColor"
                 >
                   <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 14h-2v-12h2v12zm-4 0h-2v-8h2v8zm-4 0h-2v-4h2v4z" />
